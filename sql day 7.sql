eefe0dd5abfff54769c497f2a25c5253acd3a4a8
Kusum <yadavkusum81456@gmail.com>
2025-10-19 23:24:04 +0530

    Add files via upload

---
+++ sql day 7.sql
@@ -0,0 +1,89 @@
+-- day 7 --
+
+use sprk_morning;
+
+describe student;
+
+drop table student;
+
+show tables;
+
+select now(), current_timestamp(),current_timestamp();
+
+-- table with constraints
+create table student
+(
+roll_no int primary key auto_increment,
+name varchar(100) not null,
+email varchar(100) unique not null,
+age int not null check (age between 5 and 30),
+add_date datetime not null default current_timestamp
+);
+
+describe student;
+insert into student(name,email,age)values
+("jay krishna","jay12@gamil.com","21");
+
+select * from student;
+
+-- error unique constraints fails
+insert into student(name,email,age)values
+("jay krishna","jay12@gamil.com","21");
+
+
+-- error not null
+
+insert into student(email,age)values
+("jay krishna","jay12#gmail.com","21");
+
+insert into student(name,age,email)values
+("parathmesh katkar","prath122@gmail.com","28"),
+("parth jadhav", "parth54@gmail.com","15"),
+("subh", "subh78@gmail.com","18");
+
+select * from student;
+
+insert into student(roll_no, name,email,age) values
+(30,"Mihir Yadav", "mihiryad11@yahoo.com",28);
+
+insert into student( name,email,age) values
+("Suyash Yadav", "suyash33@yahoo.com",12);
+
+select * from student;
+
+insert into student(roll_no, name,email,age) values
+(2,"Memon Abdul Gani", "abdul1@yahoo.com",28);
+select * from student;
+
+insert into student(name,email,age) values
+("Ajit Sharma", "ajit123@yahoo.com",5);
+select * from student;
+
+-- Error Code: 1062. Duplicate entry '2' for key 'student.PRIMARY'
+insert into student(roll_no, name,email,age) values
+(2,"Memon Abdul Gani", "abdul1@yahoo.com",28);
+
+-- Error Code: 3819. Check constraint 'student_chk_1' is violated.
+
+insert into student(name,email,age) values
+("Ajit Sharma", "ajit1235@gmail.com",50);
+
+
+
+select * from student;
+
+
+-- DML (UPDATE | DELETE) -> COndition is compulsory
+-- condition -> use either primary key or unique column
+
+update student
+set email = "abdul12@gmail.com"
+where roll_no = 2;
+
+select * from student;
+update student
+set email = "ajit_gupta512@gmail.com", 
+name = "Ajit Gupta", age = 25
+where roll_no = 32;
+
+select * from student;
